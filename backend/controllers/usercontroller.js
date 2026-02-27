cd04f28d17e4ebd6155d3b693ee53c875c46cd1a
Krishnavardhan2305 <captainkv.2511@gmail.com>
2026-02-27 19:03:54 +0530

    fixed same site parameter

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -108,7 +108,7 @@ export const login = async (req, res) => {
             .cookie("token", token, {
                 maxAge: 1 * 24 * 60 * 60 * 1000,
                 httpOnly: true,
-                sameSite: 'strict'
+                sameSite: 'None'
             })
             .json({
                 message: `Welcome back ${user.Name}`, 
